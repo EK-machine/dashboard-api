9b692921cbefa675600b31d5971685aaeac933f8
Yauhen Kalabukhin <coocooroozza@gmail.com>
2023-05-29 17:06:35 +0300

    added middlewares to validate register method

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'wrong email' })
 	email: string;
+
+	@IsString({ message: 'set the password' })
 	password: string;
+
+	@IsString({ message: 'set name' })
 	name: string;
 }
