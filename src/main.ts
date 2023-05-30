28fb197d946e033d8e00d995d225d53b0ed41b93
Yauhen Kalabukhin <coocooroozza@gmail.com>
2023-05-30 19:46:34 +0300

    created users repository

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ import { IUserController } from './users/users.controller.interface';
 import { IConfigService } from './config/config.service.interface';
 import { ConfigService } from './config/config.service';
 import { PrismaService } from './database/prisma.service';
+import { IUsersRepository } from './users/users.repository.interface';
+import { UsersRepository } from './users/users.repository';
 
 export interface IBootstrapReturnType {
 	appContainer: Container;
@@ -24,6 +26,7 @@ export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<IUserSevice>(TYPES.UserService).to(UserService);
 	bind<PrismaService>(TYPES.PrismaService).to(PrismaService).inSingletonScope();
 	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
+	bind<IUsersRepository>(TYPES.UsersRepository).to(UsersRepository).inSingletonScope();
 	bind<App>(TYPES.Application).to(App);
 });
 
--- src/users/user.service.interface.ts
+++ src/users/user.service.interface.ts
@@ -1,9 +1,8 @@
-import { Response, Request, NextFunction } from 'express';
 import { UserRegisterDto } from './dto/user-register.dto';
-import { User } from './user.entity';
 import { UserLoginDto } from './dto/user-login.dto';
+import { UserModel } from '@prisma/client';
 
 export interface IUserSevice {
-	createUser: (dto: UserRegisterDto) => Promise<User | null>;
+	createUser: (dto: UserRegisterDto) => Promise<UserModel | null>;
 	validateUser: (dto: UserLoginDto) => Promise<boolean>;
 }
